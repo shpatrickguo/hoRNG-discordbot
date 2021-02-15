34b8b76d1b21c0f8f0c916223ea6c0c70b677dd6
Patrick Guo <guopatrick.courses@gmail.com>
2021-02-14 21:31:11 -0800

    ping command

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = '-';
+
 client.once('ready', () => {
     console.log('hoRNG-bot is online')
+});
+
+client.on('message', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!')
+    }
 });
 
 client.login(process.env.TOKENJS);
