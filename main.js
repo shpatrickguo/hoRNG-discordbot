8943c1d9b455d225e3dd766cc754827e57e9b0e4
Patrick Guo <guopatrick.courses@gmail.com>
2021-02-14 19:32:03 -0800

    main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 
-const client = new Discord.client();
+const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('hoRNG-bot is online')
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKENJS);
