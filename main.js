94c4c276e12f2bbc4c1d11fd0b28396b6a2cc702
Patrick Guo <guopatrick.courses@gmail.com>
2021-02-15 20:27:11 -0800

    play and leave music

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({partials:["MESSAGE", "CHANNEL", "REACTION"]});
 
-const prefix = '-';
+const prefix = 'h!';
 
 const fs = require('fs');
 
@@ -50,7 +50,11 @@ client.on('message', message => {
         client.commands.get('mute').execute(message, args);
     } else if (command == 'unmute') {
         client.commands.get('unmute').execute(message, args);
-    } 
+    } else if (command == 'play') {
+        client.commands.get('play').execute(message, args);
+    } else if (command == 'leave') {
+        client.commands.get('leave').execute(message, args);
+    }
     
     //else if (command == 'reactionrole') {
     //    client.commands.get('unmute').execute(message, args, Discord, client);
