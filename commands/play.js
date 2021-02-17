2266d048941bacac8877373889df6fce49989bde
Patrick Guo <guopatrick.courses@gmail.com>
2021-02-16 18:59:08 -0800

    clarification

--- commands/play.js
+++ commands/play.js
@@ -15,7 +15,7 @@ module.exports = {
     const voice_channel = message.member.voice.channel;
     if (!voice_channel)
       return message.channel.send(
-        "You need to be in a channel to execute this command!"
+        "You need to be in a voice channel to execute this command!"
       );
     const permissions = voice_channel.permissionsFor(message.client.user);
     if (!permissions.has("CONNECT"))
