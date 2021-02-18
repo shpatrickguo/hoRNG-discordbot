0c0e7030b9444b3fb4158b5e0ac56ff1b40daec7
Patrick Guo <guopatrick.courses@gmail.com>
2021-02-17 19:09:19 -0800

    DaanMatch temp commands

---
+++ commands/questions.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "questions",
+    aliases: ["q"],
+    permissions: [],
+    description: "lists questions",
+    execute(message, args, cmd, client, Discord) {
+      const embed = new Discord.MessageEmbed()
+        .setColor("2af779")
+        .setTitle("Develop survey for NGOs/CSR")
+        .setDescription(`We want more information and data visualizations on all corporations`)
+        .setThumbnail(
+          "https://static.wixstatic.com/media/0ecae5_f1d5479031114df8ac2adcde1b44a3f4~mv2.png/v1/fill/w_466,h_160,al_c,q_85,usm_0.66_1.00_0.01/DAANG-01-1024x351.webp"
+        )
+        .setAuthor(
+          "DaanMatch",
+          "https://media-exp1.licdn.com/dms/image/C560BAQEYGvW8wH3CSw/company-logo_200_200/0/1605310347627?e=1621468800&v=beta&t=JdCjZ9wqvwcJ1nhgukP25Wz6heKDjLRrnXjdC6_y4LU",
+          "https://www.daanmatch.org/"
+        )
+        .addFields(
+          {
+            name: "What we need answered",
+            value:
+              "1. Corporate information and how they do their CSR? \n 2. Identify levels of compliance by corp size (categorized by revenue?) \n 3. Identify targets of funding by SDG \n 4. Identify any correlation between SDGs/state of the union and corp donation? \n 5. Where corps/industries are donating and where they are geographically?",
+            inline: false
+          }
+        );
+      message.channel
+        .send(embed)
+        .then((msg) => {
+          message.delete();
+        })
+        .catch((err) => {
+          throw err;
+        });
+    },
+  };
+  
---
+++ commands/set_meeting.js
@@ -0,0 +1,46 @@
+module.exports = {
+  name: "meeting",
+  aliases: ["attendance"],
+  permissions: [],
+  description: "creates a meeting",
+  execute(message, args, cmd, client, Discord) {
+    let messageArgs = args.join(" ");
+    const embed = new Discord.MessageEmbed()
+      .setColor("2af779")
+      .setTitle(`Project meeting: ${messageArgs}`)
+      .setThumbnail(
+        "https://static.wixstatic.com/media/0ecae5_f1d5479031114df8ac2adcde1b44a3f4~mv2.png/v1/fill/w_466,h_160,al_c,q_85,usm_0.66_1.00_0.01/DAANG-01-1024x351.webp"
+      )
+      .setAuthor(
+        "DaanMatch",
+        "https://media-exp1.licdn.com/dms/image/C560BAQEYGvW8wH3CSw/company-logo_200_200/0/1605310347627?e=1621468800&v=beta&t=JdCjZ9wqvwcJ1nhgukP25Wz6heKDjLRrnXjdC6_y4LU",
+        "https://www.daanmatch.org/"
+      )
+      .addFields(
+        { name: "⏲️ Meeting Length", value: "1 hour", inline: true },
+        {
+          name: "📝 Agenda",
+          value:
+            "1. Breakout room trivia \n 2. Talk over questions \n 3. Go over dataset",
+          inline: true,
+        }
+      );
+    if (cmd === "meeting") {
+      message.channel.send(embed).catch((err) => {
+        throw err;
+      });
+    } else if (cmd === "attendance") {
+      embed.setFooter("React to indicate attendance!");
+      message.channel
+        .send(embed)
+        .then((msg) => {
+          msg.react("✅");
+          msg.react("❎");
+          //message.delete();
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
+  },
+};
